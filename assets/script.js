e4b8381067060ea1d4585da317ac39493e81bae2
bcbridges <bcb35b@gmail.com>
2021-08-03 10:02:21 -0600

    Adding answer selection logic. Changed form input for li elements.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,14 @@
 var startButton = document.querySelector("#start");
 var instructions = document.querySelector(".instructions");
 var mainDisplay = document.querySelector("#mainDisplay");
+var answerChoice = document.querySelector("ol");
+var secondsRemaining = document.querySelector("#seconds");
 
+// Sets timer to stay at 75 seconds until the 'Start' button is pushed.
+var timeLeft = 75;
+secondsRemaining.textContent = timeLeft + " sec.";
+
+// Object holding questions and their properties.
 var questions = {
   question1: {
     prompt: "Question 1: DOM stands for ___ ___ ___.",
@@ -24,56 +31,60 @@ var questions = {
   // question10:
 };
 
+// Called when the game starts
 startButton.addEventListener("click", function () {
   instructions.setAttribute("style", "display: none");
   startButton.setAttribute("style", "display: none");
   createQuestion();
+  timerStart();
 });
 
+// Work on function for changing questions
+
+// Used to append the questions
 function createQuestion() {
   var prompt = document.createElement("p");
-  var opt1 = document.createElement("input");
-  var opt2 = document.createElement("input");
-  var opt3 = document.createElement("input");
-  var opt4 = document.createElement("input");
-  var lab1 = document.createElement("label");
-  var lab2 = document.createElement("label");
-  var lab3 = document.createElement("label");
-  var lab4 = document.createElement("label");
+  var opt1 = document.createElement("li");
+  var opt2 = document.createElement("li");
+  var opt3 = document.createElement("li");
+  var opt4 = document.createElement("li");
 
   prompt.textContent = questions.question1.prompt;
-  lab1.textContent = questions.question1.answers[0];
-  lab2.textContent = questions.question1.answers[1];
-  lab3.textContent = questions.question1.answers[2];
-  lab4.textContent = questions.question1.answers[3];
-
-  opt1.setAttribute("type", "radio");
-  opt2.setAttribute("type", "radio");
-  opt3.setAttribute("type", "radio");
-  opt4.setAttribute("type", "radio");
+  opt1.textContent = questions.question1.answers[0];
+  opt2.textContent = questions.question1.answers[1];
+  opt3.textContent = questions.question1.answers[2];
+  opt4.textContent = questions.question1.answers[3];
 
   opt1.setAttribute("id", "1");
   opt2.setAttribute("id", "2");
   opt3.setAttribute("id", "3");
   opt4.setAttribute("id", "4");
 
-  opt1.setAttribute("name", "option");
-  opt2.setAttribute("name", "option");
-  opt3.setAttribute("name", "option");
-  opt4.setAttribute("name", "option");
-
-  lab1.setAttribute("for", "1");
-  lab2.setAttribute("for", "2");
-  lab3.setAttribute("for", "3");
-  lab4.setAttribute("for", "4");
-
   mainDisplay.appendChild(prompt);
   mainDisplay.appendChild(opt1);
-  mainDisplay.appendChild(lab1);
   mainDisplay.appendChild(opt2);
-  mainDisplay.appendChild(lab2);
   mainDisplay.appendChild(opt3);
-  mainDisplay.appendChild(lab3);
   mainDisplay.appendChild(opt4);
-  mainDisplay.appendChild(lab4);
 }
+
+// Begins time countdown
+function timerStart() {
+  var timerInterval = setInterval(function () {
+    timeLeft--;
+    secondsRemaining.textContent = timeLeft + " sec.";
+
+    if (timeLeft === 0) {
+      clearInterval(timerInterval);
+      // Should end game after this
+    }
+  }, 1000);
+}
+
+answerChoice.addEventListener("click", function (event) {
+  var selectedAnswer = event.target.getAttribute("id");
+  var correctAnswer = questions.question1.solutionindex;
+
+  if (selectedAnswer == correctAnswer) {
+    document.querySelector("h5").textContent = "Correct!";
+  } else document.querySelector("h5").textContent = "Incorrect";
+});
