81b7a17795f48896e9471602450276f2fc257686
bcbridges <bcb35b@gmail.com>
2021-08-03 21:45:24 -0600

    Added endgame function without local storage save.

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,9 @@ var mainDisplay = document.querySelector("#mainDisplay");
 var answerChoice = document.querySelector("ol");
 var secondsRemaining = document.querySelector("#seconds");
 var currentQuestion = 0;
+var score = 0;
 // Sets timer to stay at 75 seconds until the 'Start' button is pushed.
-var timeLeft = 75;
+var timeLeft = 60;
 secondsRemaining.textContent = timeLeft + " sec.";
 
 // Object holding questions and their properties.
@@ -21,9 +22,9 @@ var questions = {
     solutionindex: 3,
   },
   question2: {
-    prompt: "Which of the following is selecting and id of 'code'?",
+    prompt: "Question 2: Which of the following is selecting an id of 'code'?",
     answers: [
-      'documnet.querySelector("#code"',
+      'documnet.querySelector("#code")',
       'documnet.querySelector(".code")',
       'document.idSelector("code")',
       'document.idSelector("#code")',
@@ -31,16 +32,6 @@ var questions = {
     solutionindex: 1,
   },
   question3: {
-    prompt: "Which of the following is selecting and id of 'q2'?",
-    answers: [
-      'documnet.querySelector("#code"',
-      'documnet.querySelector(".code")',
-      'document.idSelector("code")',
-      'document.idSelector("#code")',
-    ],
-    solutionindex: 1,
-  },
-  question4: {
     prompt: "Which of the following is selecting and id of 'q3'?",
     answers: [
       'documnet.querySelector("#code"',
@@ -50,8 +41,18 @@ var questions = {
     ],
     solutionindex: 1,
   },
-  question5: {
+  question4: {
     prompt: "Which of the following is selecting and id of 'q4'?",
+    answers: [
+      'documnet.querySelector("#code"',
+      'documnet.querySelector(".code")',
+      'document.idSelector("code")',
+      'document.idSelector("#code")',
+    ],
+    solutionindex: 1,
+  },
+  question5: {
+    prompt: "Which of the following is selecting and id of 'q5'?",
     answers: [
       'documnet.querySelector("#code"',
       'documnet.querySelector(".code")',
@@ -83,6 +84,9 @@ function changeQuestion() {
     currentQuestion = questions.question4;
   } else if (currentQuestion == questions.question4) {
     currentQuestion = questions.question5;
+  } else if (currentQuestion == questions.question5) {
+    currentQuestion = "end";
+    endGame();
   }
 }
 
@@ -100,6 +104,7 @@ function createQuestion() {
   opt3.textContent = currentQuestion.answers[2];
   opt4.textContent = currentQuestion.answers[3];
 
+  prompt.setAttribute("id", "prompt");
   opt1.setAttribute("id", "1");
   opt2.setAttribute("id", "2");
   opt3.setAttribute("id", "3");
@@ -112,13 +117,31 @@ function createQuestion() {
   mainDisplay.appendChild(opt4);
 }
 
+// Used to change the values of the questions after submit
+function updateQuestion() {
+  if (currentQuestion == "end") {
+  } else {
+    var prompt = document.getElementById("prompt");
+    var opt1 = document.getElementById("1");
+    var opt2 = document.getElementById("2");
+    var opt3 = document.getElementById("3");
+    var opt4 = document.getElementById("4");
+
+    prompt.textContent = currentQuestion.prompt;
+    opt1.textContent = currentQuestion.answers[0];
+    opt2.textContent = currentQuestion.answers[1];
+    opt3.textContent = currentQuestion.answers[2];
+    opt4.textContent = currentQuestion.answers[3];
+  }
+}
+
 // Begins time countdown
 function timerStart() {
   var timerInterval = setInterval(function () {
     timeLeft--;
     secondsRemaining.textContent = timeLeft + " sec.";
 
-    if (timeLeft === 0) {
+    if (timeLeft === 0 || currentQuestion == "end") {
       clearInterval(timerInterval);
       // Should end game after this
     }
@@ -135,4 +158,52 @@ answerChoice.addEventListener("click", function (event) {
     timeLeft -= 20;
     document.querySelector("h5").textContent = "Incorrect";
   }
+
+  changeQuestion();
+  updateQuestion();
+  setTimeout(function () {
+    document.querySelector("h5").textContent = "";
+  }, 800);
+});
+
+function endGame() {
+  var prompt = document.getElementById("prompt");
+  var opt1 = document.getElementById("1");
+  var opt2 = document.getElementById("2");
+  var opt3 = document.getElementById("3");
+  var opt4 = document.getElementById("4");
+
+  prompt.setAttribute("style", "display: none");
+  opt1.setAttribute("style", "display: none");
+  opt2.setAttribute("style", "display: none");
+  opt3.setAttribute("style", "display: none");
+  opt4.setAttribute("style", "display: none");
+  document.querySelector("h5").setAttribute("style", "display: none");
+
+  score = timeLeft;
+  var endMessage = document.createElement("h4");
+  endMessage.textContent =
+    "Thanks for playing, your score was " +
+    score +
+    ". Please enter your initials for the high scores board.";
+  mainDisplay.appendChild(endMessage);
+
+  var initials = document.createElement("input");
+  initials.setAttribute("placeholder", "Enter initials here");
+  form.appendChild(initials);
+
+  var enter = document.createElement("input");
+  enter.setAttribute("type", "submit");
+  form.appendChild(enter);
+}
+
+var form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var stats = {
+    initials: document.querySelector("input").value.trim(),
+    score: score,
+  };
+  console.log(stats);
 });
